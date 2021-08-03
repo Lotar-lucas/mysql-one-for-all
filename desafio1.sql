b68a5af250a63d0e27eef5f72c5e0563a8d727da
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-03 19:26:22 -0300

    fixed , changes made to the codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -3,25 +3,25 @@ CREATE SCHEMA SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE SpotifyClone.plano(
-	plano_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	  plano_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_plano VARCHAR(50) NOT NULL,
     valor_plano DECIMAL(3,2)
 );
 
 CREATE TABLE SpotifyClone.artista(
-	artista_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	  artista_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_artista VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE SpotifyClone.album(
-	album_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	  album_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_album VARCHAR(50) NOT NULL,
     artista_id INT(11),
     FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artista(artista_id)
 );
 
 CREATE TABLE SpotifyClone.usuario(
-	usuario_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	  usuario_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_usuario VARCHAR(50) NOT NULL,
     idade INT(3),
     plano_id INT(11),
@@ -29,7 +29,7 @@ CREATE TABLE SpotifyClone.usuario(
 );
 
 CREATE TABLE SpotifyClone.cancao(
-	cancao_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	  cancao_id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_cancao VARCHAR(50) NOT NULL,
     artista_id INT(11),
     album_id INT(11),
@@ -38,7 +38,7 @@ CREATE TABLE SpotifyClone.cancao(
 );
 
 CREATE TABLE SpotifyClone.seguidor(
-	usuario_id INT(11) NOT NULL,
+	  usuario_id INT(11) NOT NULL,
     artista_id INT(11) NOT NULL,
     CONSTRAINT PRIMARY KEY(usuario_id,artista_id),
     FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuario(usuario_id),
@@ -46,11 +46,11 @@ CREATE TABLE SpotifyClone.seguidor(
 );
 
 CREATE TABLE SpotifyClone.historico(
-	usuario_id INT(11) NOT NULL,
-	cancao_id INT(11) NOT NULL,
+	  usuario_id INT(11) NOT NULL,
+	  cancao_id INT(11) NOT NULL,
     CONSTRAINT PRIMARY KEY(usuario_id,cancao_id),
     FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuario(usuario_id),
-	FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancao(cancao_id)
+	  FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancao(cancao_id)
 );
 
 INSERT INTO plano(nome_plano, valor_plano)
