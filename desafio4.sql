a9c6172454280c253b1402f5fcaded83b6f63924
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-05 19:25:18 -0300

    fixed requirement 04 placed LIMIT

--- desafio4.sql
+++ desafio4.sql
@@ -6,4 +6,5 @@ FROM artista as a
 INNER JOIN seguidor as s
 ON s.artista_id = a.artista_id
 GROUP BY a.nome_artista
-ORDER BY count(s.artista_id) DESC, a.nome_artista;
+ORDER BY count(s.artista_id) DESC, a.nome_artista
+limit 3;
