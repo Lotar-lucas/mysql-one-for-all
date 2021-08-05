a97faa9e793196bf1f30a3475afcab82a8a8d6cc
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-05 18:52:55 -0300

    Done requirement 03

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
   u.nome_usuario as usuario,
-  h.nome_cancao as nome
+  c.nome_cancao as nome
 FROM usuario as u
 INNER JOIN historico as h
+ON u.usuario_id = h.usuario_id
+INNER JOIN cancao as c
+ON c.cancao_id = h.cancao_id
+ORDER BY u.nome_usuario, c.nome_cancao;
