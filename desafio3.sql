accaa00f180838d1d0f262fb54b71b28a85a27ea
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-05 18:26:43 -0300

    fixed for codeclimate

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-  as usuario,
-  as nome
-FROM 
+  u.nome_usuario as usuario,
+  h.nome_cancao as nome
+FROM usuario as u
+INNER JOIN historico as h
