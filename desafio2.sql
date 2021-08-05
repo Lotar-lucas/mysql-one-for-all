3cb08ea2ad77c7398a15d02853675f671f987e0e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-05 16:43:30 -0300

    done requirement 02

---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT 
+  COUNT(c.nome_cancao) as cancoes,
+  COUNT(DISTINCT artista_id) as artistas,
+  COUNT(DISTINCT album_id) as albuns
+FROM cancao as c;
+
+-- Realizado com auxilio dop colega Rafael Mathias
+
+
