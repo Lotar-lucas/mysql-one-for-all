02c19f04ffaf97c2f8097790460c5d0e83d91ade
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-05 22:16:16 -0300

    changing change made in the past commit

--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,11 @@
-
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON artista
+FOR EACH ROW
+BEGIN
+  DELETE from seguidor
+  WHERE seguidor.usuario_id = OLD.usuario_id;
+  DELETE FROM historico
+  WHERE historico.usuario_id = OLD.usuario_id;
+END$$
+DELIMITER ;
