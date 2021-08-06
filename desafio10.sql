5163c74499b52045c9e1f42e2efee85577c3e788
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-06 00:09:29 -0300

    Fixed requirement 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,12 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(id_usuario INT)
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLATE total INT;
-SELECT
-  COUNT(cancao_id)
-FROM historico
-WHERE usuario_id = id_usuario
-INTO total;
-RETURNS total;
-END $$
+	DECLARE total_music INT;
+	SELECT COUNT(usuario_id) FROM historico
+	WHERE usuario_id = id
+INTO total_music;
+RETURN total_music;
+END $$;
 DELIMITER ;
+SELECT quantidade_musicas_no_historico(3) INTO @total_musica;
