ee6840ee173e7b512439cbaeac793015b2ba1be1
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-06 00:11:09 -0300

    fixed again - codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_music INT;
-	SELECT COUNT(usuario_id) FROM historico
-	WHERE usuario_id = id
+DECLARE total_music INT;
+SELECT COUNT(usuario_id) FROM historico
+WHERE usuario_id = id
 INTO total_music;
 RETURN total_music;
 END $$;
